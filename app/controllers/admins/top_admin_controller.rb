62331e129ef5c3a9ab10f1157463066d51990492
nakamura999 <masahiro9ama9@gmail.com>
2020-05-21 11:29:47 +0000

    管理者画面管理データ作成

--- app/controllers/admins/top_admin_controller.rb
+++ app/controllers/admins/top_admin_controller.rb
@@ -4,6 +4,17 @@ class Admins::TopAdminController < ApplicationController
   layout 'admins'
 
   def index
+  	@users = User.all
   	@users_count = User.group_by_day(:created_at).size
+  	@styles = Style.all
+  	@styles_count = Style.group_by_day(:created_at).size
+  	@boards = Board.all
+  	@boards_count = Board.group_by_day(:created_at).size
+  	@jenres_count = Jenre.joins(:styles).group(:name).size
+  	@contact_count = Contact.group_by_day(:created_at).size
+  	@contact_today = Contact.where(created_at: Date.today.all_day).count
+  	@user_today = User.where(created_at: Date.today.all_day).count
+  	@style_today = Style.where(created_at: Date.today.all_day).count
+  	@board_today = Board.where(created_at: Date.today.all_day).count
   end
 end
