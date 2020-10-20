9fb5fefd297bd3cc8f4ffe4d2d0430ca63776c42
nakamura999 <masahiro9ama9@gmail.com>
2020-10-20 15:54:26 +0900

    https add to

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   # 追加 https化
-  config.force_ssl = true
+  # config.force_ssl = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
